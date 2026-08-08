0934decd250ba3181376e7286767b8a02734bc8a
Subhradeep Basu <111586851+SubhradeepBasu18@users.noreply.github.com>
2026-08-08 19:29:13 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for(char ch: s){
+            if(isalnum(ch)) str+=tolower(ch);
+        }
+
+        int l = 0, r = str.size()-1;
+
+        while(l<r){
+            if(str[l] != str[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
